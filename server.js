c128df8189a1453087eac19efff5fa8e7a2dea4d
Derin DeNio <derindenio@gmail.com>
2024-04-10 17:54:24 -0600

    Added npm dependies, set up PORT and MongodB connection

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+
+const MongoClient = require('mongodb').MongoClient
+const PORT = 2121
+require('dotenv').config()
+
+let db,
+    dbConnectionStr = process.env.DB_STRING,
+    dbName = 'artist'
+
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+    .then(client => {
+        console.log(`Connected to ${dbName} Database`)
+        db = client.db(dbName)
+    })
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+
+
+
+
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
