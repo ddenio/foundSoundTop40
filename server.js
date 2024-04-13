7bf182eebdc6d1bb5f7becd6b5a0b1472445f534
Derin DeNio <derindenio@gmail.com>
2024-04-12 18:44:58 -0600

    Added EJS file, added Add artist/song functionality, added likes/delete functionality

--- server.js
+++ server.js
@@ -21,8 +21,50 @@ app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
+app.get('/',(request, response)=>{
+    db.collection('artists').find().sort({likes: -1}).toArray()
+    .then(data => {
+        response.render('index.ejs', { info: data })
+    })
+    .catch(error => console.error(error))
+})
 
+app.post('/addArtist', (request, response) => {
+    db.collection('artists').insertOne({artistName: request.body.artistName,
+    songName: request.body.songName, likes: 0})
+    .then(result => {
+        console.log('Song Added')
+        response.redirect('/')
+    })
+    .catch(error => console.error(error))
+})
 
+app.put('/addOneLike', (request, response) => {
+    db.collection('artists').updateOne({artistName: request.body.artistNameS, songName: request.body.songNameS,likes: request.body.likesS},{
+        $set: {
+            likes:request.body.likesS + 1
+          }
+    },{
+        sort: {_id: -1},
+        upsert: true
+    })
+    .then(result => {
+        console.log('Added One Like')
+        response.json('Like Added')
+    })
+    .catch(error => console.error(error))
+
+})
+
+app.delete('/deleteArtist', (request, response) => {
+    db.collection('artists').deleteOne({artistName: request.body.artistNameS})
+    .then(result => {
+        console.log('Artist Deleted')
+        response.json('Artist Deleted')
+    })
+    .catch(error => console.error(error))
+
+})
 
 
 app.listen(process.env.PORT || PORT, () => {
